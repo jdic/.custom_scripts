a05a898a508b83542f9af596e073ccf24dd5958d
jdi <jmotadv@gmail.com>
2024-07-27 22:47:45 -0700

    [REFACTOR] Homologate the structure with the rest of the scripts

--- split.sh
+++ split.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-  echo "Usage: $0 <time> <input> [output]"
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+  echo "Usage: $0 <time> <path> [new_path]"
   exit 1
 fi
 
-time=$1
-input=$2
-output_dir=${3:-$(dirname "$input")}
+SPLIT_TIME=$1
+INPUT_PATH=$2
+OUTPUT_DIR=${3:-$(dirname "$INPUT_PATH")}
 
-basename=$(basename "$input")
-filename="${basename%.*}"
-
-if [ "$output_dir" = "." ]; then
-  output_path="./$filename"
-else
-  output_path="$output_dir/$filename"
+if [ ! -f "$INPUT_PATH" ]; then
+  echo "File not found."
+  exit 1
 fi
+
+basename=$(basename "$INPUT_PATH")
+filename="${basename%.*}"
+output_path="$OUTPUT_DIR/$filename"
 
 mkdir -p "$output_path"
 
@@ -25,4 +25,4 @@ if ! command -v ffmpeg &> /dev/null; then
   exit 1
 fi
 
-ffmpeg -i $input -c:v libx264 -c:a aac -strict experimental -b:a 192k -force_key_frames "expr:gte(t,n_forced*${time})" -f segment -segment_time $time -reset_timestamps 1 -map 0 "${output_path}/${filename}_%03d.${basename##*.}"
+ffmpeg -i "$INPUT_PATH" -c:v libx264 -c:a aac -strict experimental -b:a 192k -force_key_frames "expr:gte(t,n_forced*${SPLIT_TIME})" -f segment -segment_time "$SPLIT_TIME" -reset_timestamps 1 -map 0 "${output_path}/${filename}_%03d.${basename##*.}"
